c72dbc80ca8084de51bcd0ac878409067517767d
JESURAJA7 <jesu7raja7@gmail.com>
2026-03-05 16:09:58 +0530

    fix: remove trailing slash from CORS origin

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://mstgold.netlify.app/"],
+    origin: ["http://localhost:5173", "https://mstgold.netlify.app"],
     credentials: true,
   }),
 );
